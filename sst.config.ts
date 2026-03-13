99f3b3417507dff3af2ac331c3d5a96dc67728ec
Dan Woodbury <dan@woodbury.me.uk>
2026-03-13 12:12:10 +0000

    feat: initial commit

---
+++ sst.config.ts
@@ -0,0 +1,20 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "anti-average",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      home: "aws",
+    };
+  },
+  async run() {
+    new sst.aws.StaticSite("AntiAverageSite", {
+      path: "site",
+      domain: {
+        name: "antiaverage.studio",
+        dns: sst.aws.dns(),
+      },
+    });
+  },
+});
